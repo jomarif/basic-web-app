044ee81189b5ca3aa068770f01e89877c2897cb8
Jomari Francisco <fjomari100@gmail.com>
2024-09-17 11:27:17 -0700

    fix responsiveness

--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -34,7 +34,7 @@ const Todo = () => {
     }, [todoList, isInit])
 
     const todoDiv = todoList.map((todo, index) => (
-            <div key={index} className='bg-gray-200 p-4 shadow-2xl rounded-3xl border-black border-4 my-2 flex gap-4 w-2/5 font-bold text-3xl justify-between items-center'>
+            <div key={index} className='bg-gray-200 p-4 shadow-2xl rounded-3xl min-w-[400px] border-black border-4 my-2 flex gap-4 w-2/5 font-bold text-3xl justify-between items-center'>
                 <div className='text-center flex-1'>{todo}</div>
                 <button onClick={() => handleDelete(index)} className='bg-red-500 italic rounded-2xl border-black border-2 p-3 text-2xl'>Delete</button>
             </div>
@@ -44,11 +44,12 @@ const Todo = () => {
         <div className='flex flex-col items-center h-screen border-4 p-2 bg-gradient-to-r from-gray-600 to-gray-500'>
             <h1>Todo App</h1>
             <h2>What Needs to be done?</h2>
-            <form className='w-1/2 flex mb-3' onSubmit={handleSubmit}>
-                <input className='flex-1 border-black border-2 p-4 rounded-xl focus:outline-none text-2xl' type="text" value={textInput} onChange={(e) => setTextInput(e.target.value)}/>
-                <input className='border-black border-2 bg-gray-100 mx-2 px-2 rounded-xl p-2 w-1/6 font-bold text-2xl' type="submit" value="Add" />
+            <form className='w-1/2 min-w-[400px] flex mb-3' onSubmit={handleSubmit}>
+                <input className='flex-1 border-black w-5/6 border-2 p-4 rounded-xl focus:outline-none text-2xl' type="text" value={textInput} onChange={(e) => setTextInput(e.target.value)}/>
+                <input className='border-black border-2 bg-gray-100 ml-2 rounded-xl p-2 w-1/6 font-bold text-2xl' type="submit" value="Add" />
             </form>
             {todoDiv}
+
         </div>
     )
 };
